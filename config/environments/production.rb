1417463f85411823cf7fe02e6968c5b390e75645
Slava <fender707@rambler.ru>
2015-12-01 01:12:16 +0200

    [#109155316] - Added default url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'sec-website.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
