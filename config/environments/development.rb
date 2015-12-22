eb2f81d040b26522abbc3cf920707fbab809174a
Slava <fender707@rambler.ru>
2015-12-22 23:28:34 +0200

    Implement SendGrid for emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,11 +19,12 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
-      :address              => "smtp.gmail.com",
-      :port                 => 587,
-      :user_name            => ENV["GMAIL_USERNAME"],
-      :password             => ENV["GMAIL_PASSWORD"],
-      :authentication       => "plain",
+      :user_name => ENV['SENDGRID_USERNAME'],
+      :password => ENV['SENDGRID_PASSWORD'],
+      :domain => 'www.secventures.com',
+      :address => 'smtp.sendgrid.net',
+      :port => 587,
+      :authentication => :plain,
       :enable_starttls_auto => true
   }
   # Print deprecation notices to the Rails logger.
